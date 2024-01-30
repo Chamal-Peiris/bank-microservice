3576e525c44c5ea83dcd33aeb30ef94474f06029
chamal-peiris <chamal.peiris3g@gmail.com>
2024-01-30 21:10:06 +0530

    Create a dokcer file and integrate the basic docker build steps

---
+++ section4/accounts/src/main/java/com/chamal/accounts/mapper/AccountMapper.java
@@ -0,0 +1,20 @@
+package com.chamal.accounts.mapper;
+
+import com.chamal.accounts.dto.AccountDto;
+import com.chamal.accounts.entity.Account;
+
+public class AccountMapper {
+    public static AccountDto mapToAccountDto(Account accounts, AccountDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Account mapToAccount(AccountDto accountsDto, Account accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+}
