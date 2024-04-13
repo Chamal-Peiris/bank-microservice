acd05842c7b321e93ae7bb9ecd85ef3fb86c3fa7
Chamal-Peiris <chamal.peiris3g@gmail.com>
2024-04-13 10:12:58 +0530

    Add a logic to inject properties via @ConfigurationPropertes

--- section6/v1-springboot/accounts/src/main/java/com/chamal/accounts/controller/AccountsController.java
+++ section6/v1-springboot/accounts/src/main/java/com/chamal/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.chamal.accounts.controller;
 
 import com.chamal.accounts.constants.AccountConstants;
+import com.chamal.accounts.dto.AccountsContactInfoDto;
 import com.chamal.accounts.dto.CustomerDto;
 import com.chamal.accounts.dto.ErrorResponseDto;
 import com.chamal.accounts.dto.ResponseDto;
@@ -41,6 +42,9 @@ public class AccountsController {
 
     @Autowired
     private Environment environment;
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
     @Operation(
             summary = "Create Account REST API",
             description = "REST API to create new Customer &  Account inside EazyBank"
@@ -209,6 +213,30 @@ public class AccountsController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA17_HOME"));
+    }
+    @Operation(
+            summary = "Get Contact Info",
+            description = "Contact Info details that can be reached out in case of any issues"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountsContactInfoDto);
     }
 
 }
