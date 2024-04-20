b23caebd8d3dbdb6bba3af8c3be7fe4836184cf4
Chamal-Peiris <chamal.peiris3g@gmail.com>
2024-04-20 20:38:48 +0530

    Add basic config/profile management logics to loans and cards services

--- section6/v1-springboot/cards/src/main/java/com/chamal/CardsApplication.java
+++ section6/v1-springboot/cards/src/main/java/com/chamal/CardsApplication.java
@@ -1,6 +1,7 @@
 package com.chamal;
 
 
+import com.chamal.cards.dto.CardsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -8,11 +9,13 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Cards microservice REST API Documentation",
